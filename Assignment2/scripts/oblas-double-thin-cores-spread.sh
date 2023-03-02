921cb35a886e7014a0aa632b2cf98ee1bfc07a4a
Isac Pasianotto <93930945+IsacPasianotto@users.noreply.github.com>
2023-03-02 11:04:28 +0100

    Added some scripts

---
+++ Assignment2/scripts/oblas-double-thin-cores-spread.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --no-requeue
+#SBATCH --job-name="ex2-INTEl"
+#SBATCH --get-user-env
+#SBATCH --partition=THIN
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --exclusive
+#SBATCH --time=01:00:00
+
+cd ..
+
+module load architecture/Intel
+module load mkl
+module load openBLAS/0.3.21-omp
+
+# Needed for the BLIS library
+export LD_LIBRARY_PATH=/u/dssc/ipasia00/myblisintel/lib:$LD_LIBRARY_PATH
+
+export OMP_PLACES=cores
+export OMP_PROC_BIND=spread
+export OMP_NUM_THREADS=12
+
+
+echo m,k,n,time,GFLOPS > ./results/oblas-double-epyc-cores-spread.csv
+
+
+for size in {2000..20000..1000}
+do
+	for i in {1..15}
+	do
+		./gemm_oblas.x $size $size $size >> ./results/oblas-double-intel-cores-spread.csv
+	done
+done
---
+++ Assignment2/scripts/oblas-double-thin-size-spread.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --no-requeue
+#SBATCH --job-name="ex2-INTEL"
+#SBATCH --get-user-env
+#SBATCH --partition=THIN
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --exclusive
+#SBATCH --time=02:00:00
+
+cd ..
+
+module load architecture/Intel
+module load mkl
+module load openBLAS/0.3.21-omp
+
+# Needed for the BLIS library
+export LD_LIBRARY_PATH=/u/dssc/ipasia00/myblisintel/lib:$LD_LIBRARY_PATH
+
+export OMP_PLACES=cores
+export OMP_PROC_BIND=spread
+
+
+echo cores,m,k,n,time,GFLOPS > ./results/oblas-double-intel-size-spread.csv
+
+size=12000
+
+for i in {1..12}
+do
+	export OMP_NUM_THREADS=$i
+	for j in {1..10}
+	do
+		echo -n $i, >> ./results/oblas-double-intel-size-spread.csv
+		./gemm_oblas.x $size $size $size >> ./results/oblas-double-intel-size-spread.csv
+	done
+done
