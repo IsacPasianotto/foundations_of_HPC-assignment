a6f35d0dac7c3d90b8aef8f88c21ac95ace9fd9e
Isac Pasianotto <yh4mop12z@gmail.com>
2023-03-15 22:23:53 +0100

    strong-scal MPI

--- Assignment1/slurm-scripts/strong_mpi_scalability.sh
+++ Assignment1/slurm-scripts/strong_mpi_scalability.sh
@@ -2,12 +2,12 @@
 #SBATCH --no-requeue
 #SBATCH --job-name="MPI-s_scalability"
 #SBATCH --get-user-env
-#SBATCH --partition=EPYC
+#SBATCH --partition=THIN
 #SBATCH --nodes=2
 #SBATCH --exclusive
 #SBATCH --time=02:00:00
 
-module load architecture/AMD
+module load architecture/Intel
 module load openMPI/4.1.4/gnu/12.2.1
 
 cd ../
@@ -17,24 +17,25 @@ make clean
 make image
 make
 
-export OMP_PLACES=threads
+export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 export OMP_NUM_THREADS=1
 
 
-k=10000
+k=17500
+evolution=1
 
 ./main.x -i -k $k
 resultdir=time_measurements
 
-echo size,cores,time > $resultdir/MPI_S-scalability_size-$k.csv
+echo size,cores,time > $resultdir/MPI_S-scalability_static-ev_size-$k.csv
 
-for i in {1..128}
+for i in {1..48}
 do
     for j in {1..5}
     do
-        echo -n $k,$i, >> $resultdir/MPI_S-scalability_size-$k.csv
-        mpirun -np $i --map-by socket ./main.x -r -n 100 -e 1 -t >> $resultdir/MPI_S-scalability_size-$k.csv
+        echo -n $k,$i >> $resultdir/MPI_S-scalability_static-ev_size-$k.csv
+        mpirun -np $i --map-by core ./main.x -r -n 10 -e $evolution -t >> $resultdir/MPI_S-scalability_static-ev_size-$k.csv
     done
 done
 
